8c5d0891fc5441fea8cbca7fd8903fa83b9fbe7b
thewandamnidu <thewan2001@gmail.com>
2026-01-05 08:30:30 +0530

    Feat: Model registry related APIs are created all the callback APIs and get all APIs

---
+++ src/main/java/com/aura/clinician/api/dto/ResponseDtos/ModelRegistryListResponseDto.java
@@ -0,0 +1,17 @@
+package com.aura.clinician.api.dto.ResponseDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModelRegistryListResponseDto {
+    private List<ModelRegistryItemResponseDto> records;
+    private long totalRecords;
+}
